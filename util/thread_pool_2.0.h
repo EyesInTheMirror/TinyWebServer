73445d0915f50d40b9eb42b866d234e34c5f187f
EyesInTheMirror <1098610047@qq.com>
2023-04-09 18:47:30 +0800

    thread_pool_2.0

--- util/thread_pool_2.0.h
+++ util/thread_pool_2.0.h
@@ -1,16 +1,78 @@
 //
-// Created by mirror on 23-4-5.
+// Created by 镜瞳 on 2023/4/6.
 //
 
-#ifndef WEBSERVER_CPP11_THREAD_POOL_2_0_H
-#define WEBSERVER_CPP11_THREAD_POOL_2_0_H
+#ifndef THREAD_POOL_2_0_THREAD_POOL_H
+#define THREAD_POOL_2_0_THREAD_POOL_H
 
 #include <thread>
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <semaphore>
 
-class thread_pool {
-public:
-    explicit thread_pool(int thread_num = std::thread::hardware_concurrency());
-};
+namespace mirror {
 
+    template<typename taskType>
+    class thread_pool{
+    public:
+        explicit thread_pool(unsigned int num = std::thread::hardware_concurrency(), int max = 10000);
+        ~thread_pool();
+        bool append(taskType* task);
+    private:
+        bool stop;
+        int max_task_num;
+        std::vector<std::thread> threads;
+        std::queue<taskType*> task_queue;
 
-#endif //WEBSERVER_CPP11_THREAD_POOL_2_0_H
+        std::mutex mutex;
+        std::counting_semaphore<> sem;
+    };
+
+    template<typename taskType>
+    bool thread_pool<taskType>::append(taskType *task) {
+        {
+            std::lock_guard<std::mutex> lock(mutex);
+            if (task_queue.size() == max_task_num) {
+                mutex.unlock();
+                return false;
+            }
+            task_queue.push(task);
+        }
+        sem.release();
+        return true;
+    }
+
+    template<typename taskType>
+    thread_pool<taskType>::~thread_pool() {
+        {
+            std::lock_guard<std::mutex> lock(mutex);
+            stop = true;
+        }
+        for(auto& thread : threads) {
+            //必须引用，不能复制
+            thread.join();
+        }
+    }
+
+    template<typename taskType>
+    thread_pool<taskType>::thread_pool(unsigned int num, int max) :stop(false), max_task_num(max){
+        for(int i = 0; i < num; ++i) {
+            threads.emplace_back([this]{
+                while(!stop || !task_queue.empty()) {
+                    taskType* task;
+                    {
+                        sem.acquire();
+                        std::unique_lock<std::mutex> lock(mutex);
+                        if(stop && task_queue.empty()) return;
+                        task = task_queue.front();
+                        task_queue.pop();
+                    }
+                    task->process();
+                }
+            });
+        }
+    }
+}
+
+#endif //THREAD_POOL_2_0_THREAD_POOL_H
